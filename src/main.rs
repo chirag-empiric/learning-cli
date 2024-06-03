2ebc1108b2fe1897b5f8ce0c66e40ce1abb1cd57
chirag-empiric <chirag@empiricinfotech.com>
2024-06-03 15:45:52 +0530

    feat: ch2 done

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)] // Read from `Cargo.toml`
 struct Cli {
-    one: Vec<String>,
+    #[command(subcommand)]
+    command: Commands,
+    #[arg(default_value = "darwin")]
+    name: String,
 }
 
+#[derive(Subcommand)]
+enum Commands {
+    Add(AddArgs),
+}
+
+#[derive(Args)]
+struct AddArgs {
+    n1: u8,
+    n2: u8,
+}
 fn main() {
     let cli = Cli::parse();
-    println!("One: {:?}", cli.one);
+    println!("Name is: {}", cli.name);
+
+    match &cli.command {
+        Commands::Add(args) => {
+            println!("Addition is: {:?}", args.n1 + args.n2);
+        }
+    }
 }
